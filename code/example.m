87cbefaf56ab07f4ff7d4dfd928ebb954f48c038
krawtchouk <140281720+krawtchouk@users.noreply.github.com>
2024-01-11 15:49:08 +0000

    Update example.m

--- code/example.m
+++ code/example.m
@@ -1,4 +1,10 @@
-path = '';
+path = '../example_data/airplane.im';
 dim = 128;
 img = readImage(path, dim);
-invariants = get3DKMI(img, dim);
+invariants1 = get3DKMI(img, dim);
+
+%%
+path = '../example_data/cup.im';
+dim = 128;
+img = readImage(path, dim);
+invariants2 = get3DKMI(img, dim);
